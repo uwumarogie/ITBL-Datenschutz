ae9e59b5364be4eece25d37c404a4e1f1f0ba9e9
phillipsuwumarogie <phillips.uwumarogie@tum.de>
2024-05-02 14:03:07 +0200

    fix: pipeline

--- src/app/space/layout.tsx
+++ src/app/space/layout.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Section } from "@/app/space/page";
 import { UnlockMasterQuiz } from "@/components/UnlockMasterQuiz";
+import {Section} from "@/components/Section";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
