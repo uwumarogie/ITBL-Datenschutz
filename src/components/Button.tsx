8cdb913f3e58876ed39946a60a846e7b25c3cfdd
walamana <kontakt@walamana.de>
2024-05-10 20:43:43 +0200

    fix: move button style from css to Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import {MouseEventHandler, PropsWithChildren} from "react";
+import clsx from "clsx";
 
 type ButtonProps = {
     onClick?: MouseEventHandler<HTMLButtonElement> | undefined,
@@ -8,7 +9,8 @@ type ButtonProps = {
 } & PropsWithChildren
 
 export default function Button({children, onClick, className}: ButtonProps) {
+    const buttonStyle = "inline-flex justify-center align-center bg-orange-500 px-6 py-3 text-white font-medium rounded-2xl transition-colors "
     return (
-        <button className={"button transition-colors " + className} onClick={onClick}>{children}</button>
+        <button className={clsx(buttonStyle, className)} onClick={onClick}>{children}</button>
     )
 }
