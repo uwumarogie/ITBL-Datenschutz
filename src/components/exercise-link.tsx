d45de50f0851500129decee4c101f19173cd2010
yannickwestermann <yannick.westermann@tum.de>
2024-05-30 19:38:52 +0200

    module description

--- src/components/exercise-link.tsx
+++ src/components/exercise-link.tsx
@@ -1,22 +1,28 @@
-import Link from "next/link";
+"use client";
 import Image from "next/image";
 import { displayText } from "@/util/landing-page";
+import Button from "./button";
+import { useRouter } from "next/navigation";
 
 type ExerciseLink = {
   slug: string;
   text: string;
   imageSrc: string;
+  description: string;
 };
-export default function ExerciseLink({ slug, text, imageSrc }: ExerciseLink) {
+export default function ExerciseLink({
+  slug,
+  text,
+  imageSrc,
+  description,
+}: ExerciseLink) {
   const modifiedText = displayText(text);
+  const router = useRouter();
   return (
-    <div className="flex flex-col justify-center h-full">
-      <Link
-        href={slug}
-        className="flex justify-center bg-sky-200 rounded-2xl p-2 max-h-[290px] min-h-[240px] h-full"
-      >
-        <div className="flex flex-col justify-center">
-          <h3 className="text-center text-blue-background text-xl">
+    <div className="flex flex-col justify-center h-full rounded-2xl">
+      <div className="relative group flex justify-center bg-sky-200 rounded-2xl p-2 max-h-[290px] min-h-[240px] h-full">
+        <div className="flex flex-col justify-between">
+          <h3 className="text-center text-blue-background text-xl h-[56px]">
             {modifiedText}
           </h3>
           <Image
@@ -27,7 +33,15 @@ export default function ExerciseLink({ slug, text, imageSrc }: ExerciseLink) {
             className="mx-auto"
           />
         </div>
-      </Link>
+        <div className="absolute inset-0 h-full flex items-end">
+          <div className="overflow-hidden rounded-2xl w-full h-full max-h-[77%]">
+            <div className="flex flex-col justify-between px-8 pb-3 pt-6 w-full h-full bg-sky-200 bg-opacity-100 opacity-0 group-hover:animate-slideUp group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+              <p className="text-blue-background text-center">{description}</p>
+              <Button onClick={() => router.push(slug)}>Modul starten</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
         "module-blue": "#A9D6E5",
       },
       height: {
-        "reduced-40": "calc(100% - 43px)",
+        "reduced-40": "calc(100% - 38px)",
         "reduced-safari": "calc(100vh - 50px)",
       },
     },
@@ -28,10 +28,20 @@ const config: Config = {
         "0%": { transform: "translateX(0)" },
         "100%": { transform: "translateX(100%)" },
       },
+      slideUp: {
+        "0%": { transform: "translateY(100%)" },
+        "100%": { transform: "translateY(0)" },
+      },
+      slideDown: {
+        "0%": { transform: "translateY(-100%)" },
+        "100%": { transform: "translateY(0)" },
+      },
     },
     animation: {
       slideIn: "slideIn 0.5s ease-out forwards",
       slideOut: "slideOut 0.5s ease-out forwards",
+      slideUp: "slideUp 0.5s ease-out forwards",
+      slideDown: "slideDown 0.5s ease-out forwards",
     },
   },
   plugins: [],
