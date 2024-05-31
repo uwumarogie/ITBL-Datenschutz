7a62ed04403f783893d9749b3df399d19e49d7df
walamana <kontakt@walamana.de>
2024-06-01 01:49:52 +0200

    feat: animate between page changes

--- src/app/(safespace)/layout.tsx
+++ src/app/(safespace)/layout.tsx
@@ -1,5 +1,11 @@
+"use client";
+
 import { DesktopNav } from "@/components/NavBar/DesktopNavigation/desktop-nav";
 import { MobileNav } from "@/components/NavBar/MobileNavigation/mobile-nav";
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
+import React, { useContext, useRef } from "react";
+import { LayoutRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 export default function Layout({
   children,
@@ -18,9 +24,42 @@ export default function Layout({
         </div>
 
         <div className="flex flex-row justify-center grow min-w-[220px] overflow-hidden">
-          <div className="bg-white rounded-3xl py-6 w-full">{children}</div>
+          <div className="bg-white rounded-3xl py-6 w-full">
+            <Transition>{children}</Transition>
+          </div>
         </div>
       </div>
     </div>
+  );
+}
+
+function FrozenRouter(props: { children: React.ReactNode }) {
+  const context = useContext(LayoutRouterContext ?? {});
+  const frozen = useRef(context).current;
+
+  return (
+    <LayoutRouterContext.Provider value={frozen}>
+      {props.children}
+    </LayoutRouterContext.Provider>
+  );
+}
+
+function Transition({ children }: { children: React.ReactNode }) {
+  const key = usePathname();
+  return (
+    <React.Fragment>
+      <AnimatePresence initial={false} mode="popLayout">
+        <motion.div
+          key={key}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { delay: 0.6 } }}
+          exit={{ opacity: 0 }}
+          transition={{ ease: "easeInOut", delay: 0.3 }}
+          className="overflow-hidden"
+        >
+          <FrozenRouter>{children}</FrozenRouter>
+        </motion.div>
+      </AnimatePresence>
+    </React.Fragment>
   );
 }
