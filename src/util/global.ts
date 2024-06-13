17a9bcd27f302c1be4d21c23851268fca1f197ab
phillipsuwumarogie <phillips.uwumarogie@tum.de>
2024-06-13 17:19:38 +0200

    change production url

--- src/util/global.ts
+++ src/util/global.ts
@@ -1,6 +1,4 @@
-const productionUrl = "https://itbl-datenschutz.vercel.app/";
-
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? productionUrl
+    ? process.env.PRODUCTION_URL
     : process.env.NEXT_PUBLIC_API_BASE_URL;
