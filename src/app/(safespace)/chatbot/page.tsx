171daf69a2fa93c7ebb49657eb3a48294cd06138
phillipsuwumarogie <phillips.uwumarogie@tum.de>
2024-06-13 14:50:35 +0200

    format: code

--- src/app/(safespace)/chatbot/page.tsx
+++ src/app/(safespace)/chatbot/page.tsx
@@ -2,5 +2,9 @@
 import { PrivacyQuiz } from "@/components/privacy-quiz";
 
 export default function Chatbot() {
-  return <div className="p-6">habdhshbfsvfefelufelfoebloeurbeuogbeugbeugberubeugbergubegu</div>;
+  return (
+    <div className="p-6">
+      habdhshbfsvfefelufelfoebloeurbeuogbeugbeugberubeugbergubegu
+    </div>
+  );
 }
