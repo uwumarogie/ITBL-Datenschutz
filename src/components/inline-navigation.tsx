6508113e784628a480f313d2776713108850cf22
yannickwestermann <yannick.westermann@tum.de>
2024-06-18 15:20:11 +0200

    inline nav update

--- src/components/inline-navigation.tsx
+++ src/components/inline-navigation.tsx
@@ -1,24 +1,28 @@
 import { NavButton, NavButtonType } from "@/components/nav-button";
+import { PersistUserService } from "@/services/user/PersistUserService";
+import { CloudArrowDown, FishSimple, Lightbulb, LockKey, Password, Scales } from "@phosphor-icons/react";
+import { useEffect } from "react";
 
 const navButtons: Omit<NavButtonType, "number">[] = [
-  { href: "/space/intro", isFinished: false },
-  { href: "/space/passwort", isFinished: false },
-  { href: "/space/privatsphaere", isFinished: false },
-  { href: "/space/daten-verarbeitung", isFinished: false },
-  { href: "/space/phishing", isFinished: false },
-  { href: "/space/rechte", isFinished: false },
+  { href: "/space/intro", isFinished: false, icon: <Lightbulb size={20}/>, description: "Modul 1 - Intro"},
+  { href: "/space/passwort", isFinished: false, icon: <Password size={28}/>, description: "Modul 2 - Passwort"},
+  { href: "/space/privatsphaere", isFinished: false, icon: <LockKey size={20}/>, description: "Modul 3 - Privatsphäre"},
+  { href: "/space/daten-verarbeitung", isFinished: false, icon: <CloudArrowDown size={28}/>, description: "Modul 4 - Datenverarbeitung"},
+  { href: "/space/phishing", isFinished: false, icon: <FishSimple size={28}/>, description: "Modul 5 - Phishing"},
+  { href: "/space/rechte", isFinished: false, icon: <Scales size={28}/>, description: "Modul 6 - Meine Rechte"},
 ];
 
-export function InlineNavigation() {
+export async function InlineNavigation() {
   return (
     <div className="max-w-[700px] sm:pb-6">
       <div className="grid grid-cols-3 gap-2 sm:flex sm:flex-row sm:justify-between sm:w-full">
-        {navButtons.map(({ href, isFinished }, index) => (
+        {navButtons.map(({ href, isFinished, icon, description }) => (
           <NavButton
             key={href}
-            number={index + 1}
             href={href}
             isFinished={isFinished}
+            icon={icon}
+            description={description}
           />
         ))}
       </div>
--- src/components/nav-button.tsx
+++ src/components/nav-button.tsx
@@ -4,25 +4,30 @@ import Link from "next/link";
 import Image from "next/image";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export type NavButtonType = {
   href: string;
   isFinished: boolean;
-  number: number;
+  icon: React.ReactNode;
+  description: string;
 };
 
-export function NavButton({ href, isFinished, number }: NavButtonType) {
+export function NavButton({ href, isFinished, icon, description }: NavButtonType) {
   const router = usePathname();
   let routeLeaf = href.split("/").pop() || href;
   const active = router.includes(routeLeaf);
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <Link
       className={clsx(
-        "flex min-w-12 min-h-12 sm:min-w-8 sm:min-h-8 md:min-w-12 md:min-h-12 rounded-xl bg-blue-background font-bold text-white justify-center items-center cursor-pointer transition-colors duration-150",
+        "relative flex min-w-12 min-h-12 sm:min-w-8 sm:min-h-8 md:min-w-12 md:min-h-12 rounded-xl bg-blue-background font-bold text-white justify-center items-center cursor-pointer transition-colors duration-150",
         active && "bg-orange-600",
       )}
       href={href}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
       <span className="flex">
         {isFinished ? (
@@ -33,9 +38,15 @@ export function NavButton({ href, isFinished, number }: NavButtonType) {
             width={20}
           />
         ) : (
-          number
+          icon
         )}
       </span>
+      <div
+        className="hidden sm:block absolute left-3 top-[50px] px-2 py-1 bg-gray-700 text-white text-xs min-w-[130px] rounded transition-opacity duration-300 z-50"
+        style={{ opacity: isHovered ? 1 : 0 }}
+      >
+        {description}
+      </div>
     </Link>
   );
 }
