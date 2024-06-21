cbaa24d59652ddd8c125b9f9b57133227ab86f08
yannickwestermann <yannick.westermann@tum.de>
2024-06-21 19:53:41 +0200

    sss

--- src/util/achievement-data.tsx
+++ src/util/achievement-data.tsx
@@ -10,6 +10,12 @@ export enum AchievementId {
   DATENSCHUTZ_HELD = "DATENSCHUTZ_HELD",
   WERBE_GURU = "WERBE_GURU",
   PHISHING_FAENGER = "PHISHING_FAENGER",
+  INTRO_FINISHED = "INTRO_FINISHED",
+  PASSWORT_FINISHED = "PASSWORT_FINISHED",
+  PRIVATSPHAERE_FINISHED = "PRIVATSPHAERE_FINISHED",
+  DATENVERARBEITUNG_FINISHED = "DATENVERARBEITUNG_FINISHED",
+  PHISHING_FINISHED = "PHISHING_FINISHED",
+  MEINE_RECHTE_FINISHED = "MEINE_RECHTE_FINISHED",
   PASSWORD_QUIZ = "PASSWORD_QUIZ",
 }
 
@@ -19,7 +25,7 @@ export namespace AchievementData {
       id: AchievementId.DATENSCHUTZ_HELD,
       title: "Datenschutz-Held",
       description:
-        "Du bist jetzt in seinem Gebiet ein Profi. Kläre eine Person in deinem Umfeld auf und überwiege sie ein sicheres Passwort zu erstellen.",
+        "Du bist jetzt in deinem Gebiet ein Profi. Kläre eine Person in deinem Umfeld auf und überzeuge sie, ein sicheres Passwort zu erstellen.",
       progress: false,
     },
     {
@@ -32,6 +38,42 @@ export namespace AchievementData {
       id: AchievementId.PHISHING_FAENGER,
       title: "Phishing-Fänger",
       description: "Erkenne und melde 2 Phishing-Versuche.",
+      progress: true,
+    },
+    {
+      id: AchievementId.INTRO_FINISHED,
+      title: "Einsteiger",
+      description: "Schließe das Intromodul erfolgreich ab",
+      progress: true,
+    },
+    {
+      id: AchievementId.PASSWORT_FINISHED,
+      title: "Passwort-Profi",
+      description: "Schließe das Passwortmodul erfolgreich ab",
+      progress: true,
+    },
+    {
+      id: AchievementId.PRIVATSPHAERE_FINISHED,
+      title: "Privatsphäre-Protektor",
+      description: "Schließe das Privatsphäremodul erfolgreich ab",
+      progress: true,
+    },
+    {
+      id: AchievementId.DATENVERARBEITUNG_FINISHED,
+      title: "Datenverarbeitungs-Diplomat",
+      description: "Schließe das Datenverarbeitungsmodul erfolgreich ab",
+      progress: true,
+    },
+    {
+      id: AchievementId.PHISHING_FINISHED,
+      title: "Phishing-Abwehrer",
+      description: "Schließe das Phishingmodul erfolgreich ab",
+      progress: true,
+    },
+    {
+      id: AchievementId.MEINE_RECHTE_FINISHED,
+      title: "Rechte-Ratgeber",
+      description: "Schließe das \"Meine Rechte\"-Modul erfolgreich ab",
       progress: true,
     },
     {
