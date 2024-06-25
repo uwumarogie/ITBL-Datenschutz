ce83e4eacdcd31f8891da62e55e4e15de229c6a4
yannickwestermann <yannick.westermann@tum.de>
2024-06-25 12:16:21 +0200

    show achievement

--- src/app/(safespace)/space/intro/page.tsx
+++ src/app/(safespace)/space/intro/page.tsx
@@ -1,11 +1,23 @@
 "use client";
 
+import Button from "@/components/button";
 import { InlineNavigation } from "@/components/inline-navigation";
 import { VideoPlayer } from "@/components/video-player";
+import { useMessages } from "@/services/notfication/message-provider";
+import { PersistUserService } from "@/services/user/PersistUserService 2";
+import { AchievementId } from "@/util/achievement-data";
 
 export default function Intro() {
+  const messageService = useMessages()
+  const handleClick = () => {
+    const userService = new PersistUserService()
+
+    userService.setAchievement(AchievementId.INTRO_FINISHED, true)
+    messageService.showAchievement(AchievementId.INTRO_FINISHED)
+  }
+
   return (
-    <div className="px-6">
+    <div className="flex flex-col px-6">
       <div className="hidden sm:block">
         <InlineNavigation />
       </div>
@@ -21,6 +33,9 @@ export default function Intro() {
           width={1280}
         />
       </div>
+      <Button onClick={() => handleClick()} className="max-w-[200px] mt-8">
+        Abschließen
+      </Button>
     </div>
   );
 }
