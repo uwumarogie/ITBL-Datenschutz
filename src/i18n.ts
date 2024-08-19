0b01331f7c5e8be8b356c375657e98b2c9249a29
walamana <kontakt@walamana.de>
2024-08-19 17:16:04 +0200

    fix: use de as fallback language

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import {getRequestConfig} from 'next-intl/server';
 import { cookies } from 'next/headers';
  
 export default getRequestConfig(async () => {
-  const locale = cookies().get('NEXT_LOCALE')?.value;
+  const locale = cookies().get('NEXT_LOCALE')?.value ?? "de";
  
   return {
     locale,
